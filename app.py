f8e04506e68c143ebbda9b62bd4a7c354f006a2e
Jayvee Mapote <jnmapote@up.edu.ph>
2026-04-30 15:42:50 +0800

    Flask app running in WSL

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("WanderWallet setup")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "WanderWallet Flask is working!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
