7fd0e1f12ce934c43f2945617bd6128796797552
Jayvee Mapote <jnmapote@up.edu.ph>
2026-05-17 12:26:32 +0800

    add port

--- app.py
+++ app.py
@@ -551,5 +551,5 @@ def ai_insights():
         return jsonify({'error': str(exc)}), 500
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
